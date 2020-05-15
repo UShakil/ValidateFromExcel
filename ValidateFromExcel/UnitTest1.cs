3e037d879b78735f211412574d9a23667a1255a4
UShakil <umar.shakil90@outlook.com>
2020-05-15 10:46:15 +0100

    - Fixed an issue where mismatch between two sheets were not being picked up - AssertFailedException added to assist with mismatch finding

--- ValidateFromExcel/UnitTest1.cs
+++ ValidateFromExcel/UnitTest1.cs
@@ -1,13 +1,12 @@
-﻿using System;
+﻿#region Using
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ExcelDataReader.Core;
-using ExcelDataReader.Log; 
 using System.IO;
 using ExcelDataReader;
 using System.Data;
-using System.Collections;
 using System.Collections.Generic;
 
+#endregion
 namespace ValidateFromExcel
 {
     [TestClass]
@@ -16,16 +15,20 @@ namespace ValidateFromExcel
         [TestMethod]
         public void ReadFromExcelSheet()
         {
-
+            #region ColumnHeaders
             string[] columnHeaders = {"Journal Type", "Account Code", "Accounting Period", "Year of Account", "Transaction Date",
                 "Settlement Currency", "Settlement Amount", "Origial Currency", "Original Amount", "Debit/Credit",
                 "Trading Partner/Counterparty (Incl Branc  Analysis Code", "Risk Code Analysis Code",
                 "Country of Insured Item  Analysis Code", "Transaction Code"
             };
+            #endregion
 
+            #region FileStreams
             FileStream streamActualResult = File.Open(@"C:\Users\umars\Desktop\LoL\ActualResult.csv", FileMode.Open, FileAccess.Read);
             FileStream streamExpectedResult = File.Open(@"C:\Users\umars\Desktop\LoL\ExpectedResult.csv", FileMode.Open, FileAccess.Read);
+            #endregion
 
+            #region DataSets
             IExcelDataReader excelDataReader1;
             excelDataReader1 = ExcelReaderFactory.CreateCsvReader(streamActualResult);
 
@@ -34,42 +37,41 @@ namespace ValidateFromExcel
             IExcelDataReader excelDataReader2;
             excelDataReader2 = ExcelReaderFactory.CreateCsvReader(streamExpectedResult);
 
-            DataSet expectedDataSet = excelDataReader1.AsDataSet();
+            DataSet expectedDataSet = excelDataReader2.AsDataSet();
+            #endregion
 
+            #region LoadLists
             List<string> actualList = ReadDataSheet(actualDataSet, columnHeaders);
 
             List<string> expectedList = ReadDataSheet(expectedDataSet, columnHeaders);
 
+            #endregion
+
+            #region DisplayOutput
             for (int i = 0; i < actualList.Count; i++)
             {
-                Assert.AreEqual(expectedList[i], actualList[i], $"Expected Value: {expectedList[i]}, Actual Value: {actualList[i]} ");
-                Console.WriteLine("SUCCESS!!!");
-                Console.WriteLine("Expected Result Row Value:       " + expectedList[i]);
-                Console.WriteLine("Actual Result Row Value:           " + actualList[i]);
+                try
+                {
+                    ConsoleWriteLine($"----- Validating row {i + 1} between Expected and Actual sheet!! -----");
+                    Assert.AreEqual(expectedList[i], actualList[i]);
+                    ConsoleWriteLine("SUCCESS!!!");
+                    ConsoleWriteLine("Expected Result Row Value:       " + expectedList[i]);
+                    ConsoleWriteLine("Actual Result Row Value:           " + actualList[i]);
 
-                Console.WriteLine("\n\n");
+                    ConsoleWriteLine("\n\n");
+                }
+                catch (AssertFailedException e)
+                {
+                    ConsoleWriteLine($"FAILURE!!! There is a mismatch at row: {i + 1}");
+                    ConsoleWriteLine(e.Message);
+                    Assert.Fail();                 
+                }
             }
-
-            //Assert.AreEqual(expectedDataSet, actualDataSet);
-
-            //foreach (var item in expectedList)
-            //{
-            //    Console.WriteLine(item + "   ");
-            //}
-
-            //Console.WriteLine("\n\n");
-
-            //foreach (var item in actualList)
-            //{
-            //    Console.WriteLine(item + "   ");
-            //}
-
+            #endregion
         }
 
         private List<string> ReadDataSheet(DataSet dataset, string[] columnHeaders)
         {
-
-            //dataset.Tables[0].DefaultView.Sort = ""
             var list = new List<string>();
 
             string currentRowData = null;
@@ -80,7 +82,6 @@ namespace ValidateFromExcel
                 for (int numberOfColumns = 0; numberOfColumns < columnHeaders.Length; numberOfColumns++)
                 {
                     currentRowData += string.Concat("|", GetColumnValueFromExcel(dataset, rowNumber, columnHeaders[numberOfColumns]));
-                    //list.Add(GetColumnValueFromExcel(result, rowNumber, columnHeaders[numberOfColumns]));
                 }
 
                 list.Add(currentRowData);
@@ -101,5 +102,7 @@ namespace ValidateFromExcel
             }
             return columnValue;
         }
+
+        private void ConsoleWriteLine(string output) => Console.WriteLine(DateTime.Now.ToString("hh:mm:ss") + " -- " + output);
     }
 }
